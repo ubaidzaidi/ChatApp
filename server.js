20cd9be41cd9a14a80a2d23f198ecf79a3b79c5c
ubaidzaidi <ubaidzaidi1230@gmail.com>
2022-03-05 18:03:32 +0530

    first but second

---
+++ server.js
@@ -0,0 +1,21 @@
+const io = require("socket.io")(8000,{
+    cors:{
+        origin:'*',
+    }
+})
+
+const user = {}
+
+io.on('connection',socket=>{
+    socket.on('new-user-joined',name=>{
+        user[socket.id]=name
+        socket.broadcast.emit('user-joined',name)
+    })
+    socket.on("send",message=>{
+        socket.broadcast.emit("receive",{"message":message,name:user[socket.id]})
+    })
+    socket.on("disconnect",message=>{
+        socket.broadcast.emit("user-left",user[socket.id])
+        delete user[socket.id]
+    })
+})
